6881938a1e6df077b99d246ce767ad6cf9512295
Vishvika Shukla <vishvika@Vishvikas-MacBook-Air.local>
2024-05-20 16:54:27 +0530

    Initial commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,36 @@
+// const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+// function handleSelectSquare(rowIndex, colIndex) {
+//   setGameBoard((prevGameBoard) => {
+//     const updatedBoard = [
+//       ...prevGameBoard.map((innerArrays) => [...innerArrays]),
+//     ];
+//     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+//     return updatedBoard;
+//   });
+
+//   onSelectSquare();
+// }
+
+export default function GameBoard({ onSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
